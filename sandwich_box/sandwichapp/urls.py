02d94d66080a7b7a09a627bbfdb6b9320da9523b
storrer <storrer.chris@gmail.com>
2022-04-14 21:14:59 -0700

    Update comments in sandwichapp/ files.

--- sandwich_box/sandwichapp/urls.py
+++ sandwich_box/sandwichapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from sandwichapp.views import SandwichappView
+from sandwichapp.views import SandwichappView, IngredientsView, SandwichGeneratorView
 
 urlpatterns = [
+    # Main Sandwich app page
     path('', SandwichappView.as_view(), name='sandwich'),
+    # Ingredient list page
+    path('ingredients/<str:ingredient_type>', IngredientsView.as_view(), name='ingredients_list'),
+    # Random sandwich generation page
+    path('random', SandwichGeneratorView.as_view(), name='sandwich_generator'),
 ]
--- sandwich_box/sandwichapp/views.py
+++ sandwich_box/sandwichapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 from django.views import View
+from django.http import Http404
 import random
+
+# Ingredients data dictionary
 ingredients = {
     'meats': ['ham', 'salami', 'turkey', 'chicken', 'meatball', 'tempeh'],
-    'cheeses': ['cheddar', 'provolone', 'swiss', 'american', 'gruyere'],
+    'cheeses': ['cheddar', 'provolone', 'swiss', 'american', 'gruyere', 'mozzarella'],
     'toppings': ['lettuce', 'tomato', 'pickles', 'onions', 'peppers'],
     'dough': ['hand tossed', 'thin crust', 'deep dish']
 }
@@ -12,8 +15,22 @@ ingredients = {
 # Create your views here.
 class SandwichappView(View):
     def get(self, request):
-        return render(request, 'sandwichapp.html')
-class SandwichGenerator(View):
-    def get(self,reqiest):
-        return
-# create view for sandwich, view for generated sandwich, and the html page for the sandwich generator
+        # pass the keys to the ingredients dictionary to the template
+        context = {'ingredients': ingredients.keys()}
+        return render(request, 'sandwichapp.html', context=context)
+
+class IngredientsView(View):
+    def get(self, request, ingredient_type):
+        if request.method == 'GET':
+            if ingredient_type not in ingredients:
+                raise Http404(f'No such ingredient: {ingredient_type}')
+
+            return render(
+                request = request,
+                template_name = 'ingredients_list.html',
+                context={ 'ingredients': ingredients[ingredient_type],
+                            'ingredient_type': ingredient_type }
+            )
+
+class SandwichGeneratorView(View):
+    pass
